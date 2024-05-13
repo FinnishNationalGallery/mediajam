8d81cee98a129923e41d1c55ce3bdf146821bf75
Kari Peiponen <kari.peiponen@fng.fi>
2024-05-13 13:05:28 +0300

    Delete sign.sh

--- sign.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd $2
-TMPFILE=`mktemp` || exit 1
-sum=`openssl dgst -sha1 mets.xml | awk -F '= ' '{print $NF}'`
-echo ./mets.xml:sha1:$sum > $TMPFILE
-openssl smime -sign -signer $1 -in $TMPFILE > signature.sig
-rm $TMPFILE
